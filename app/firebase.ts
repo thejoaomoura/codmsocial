7735286852f652baab1a316623e44a4b8ce76873
João V. Moura <73629399+thejoaomoura@users.noreply.github.com>
2025-10-15 08:45:44 -0300

    Atualizar a chaves e a lógica de inicialização do Firebase

--- app/firebase.ts
+++ app/firebase.ts
@@ -1,21 +1,19 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
+// src/app/firebase.ts
+import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-// Chave Principal do Firebase do Projeto
 const firebaseConfig = {
-  apiKey: "AIzaSyC9b62CSL3fEwR2Z0s5757B3aXvrkhfvK4",
-  authDomain: "codmsocial-e2649.firebaseapp.com",
-  projectId: "codmsocial-e2649",
-  storageBucket: "codmsocial-e2649.firebasestorage.app",
-  messagingSenderId: "753477650265",
-  appId: "1:753477650265:web:2209a48126e572c824ae48"
+  apiKey: "AIzaSyB3LeSAftV91bja13PSOQkTTsIrg4faMBk",
+  authDomain: "codmsocial-f8015.firebaseapp.com",
+  projectId: "codmsocial-f8015",
+  storageBucket: "codmsocial-f8015.firebasestorage.app",
+  messagingSenderId: "391686792657",
+  appId: "1:391686792657:web:a5809f64ef1c9a61ecacf0"
 };
 
-// Inicializa o Firebase apenas se não existir uma instância
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
-const db = getFirestore(app);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+const app = initializeApp(firebaseConfig);
 
-export { db, auth, provider };
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
